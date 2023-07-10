8477e82e2e66fb2973127feab049698ca9bda56d
coubs <john.mccoubrie15@gmail.com>
2023-07-10 13:12:49 -0400

    Overlay Class

---
+++ IdleAdventure/Intermediate/Build/Win64/UnrealEditor/Inc/IdleAdventure/UHT/OverlayWidgetController.gen.cpp
@@ -0,0 +1,89 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "IdleAdventure/Public/UI/WidgetController/OverlayWidgetController.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeOverlayWidgetController() {}
+// Cross Module References
+	IDLEADVENTURE_API UClass* Z_Construct_UClass_UIdleWidgetController();
+	IDLEADVENTURE_API UClass* Z_Construct_UClass_UOverlayWidgetController();
+	IDLEADVENTURE_API UClass* Z_Construct_UClass_UOverlayWidgetController_NoRegister();
+	UPackage* Z_Construct_UPackage__Script_IdleAdventure();
+// End Cross Module References
+	void UOverlayWidgetController::StaticRegisterNativesUOverlayWidgetController()
+	{
+	}
+	IMPLEMENT_CLASS_NO_AUTO_REGISTRATION(UOverlayWidgetController);
+	UClass* Z_Construct_UClass_UOverlayWidgetController_NoRegister()
+	{
+		return UOverlayWidgetController::StaticClass();
+	}
+	struct Z_Construct_UClass_UOverlayWidgetController_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UECodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_UOverlayWidgetController_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_UIdleWidgetController,
+		(UObject* (*)())Z_Construct_UPackage__Script_IdleAdventure,
+	};
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UOverlayWidgetController_Statics::Class_MetaDataParams[] = {
+		{ "Comment", "/**\n * \n */" },
+		{ "IncludePath", "UI/WidgetController/OverlayWidgetController.h" },
+		{ "ModuleRelativePath", "Public/UI/WidgetController/OverlayWidgetController.h" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_UOverlayWidgetController_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<UOverlayWidgetController>::IsAbstract,
+	};
+	const UECodeGen_Private::FClassParams Z_Construct_UClass_UOverlayWidgetController_Statics::ClassParams = {
+		&UOverlayWidgetController::StaticClass,
+		nullptr,
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		UE_ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x001000A0u,
+		METADATA_PARAMS(Z_Construct_UClass_UOverlayWidgetController_Statics::Class_MetaDataParams, UE_ARRAY_COUNT(Z_Construct_UClass_UOverlayWidgetController_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_UOverlayWidgetController()
+	{
+		if (!Z_Registration_Info_UClass_UOverlayWidgetController.OuterSingleton)
+		{
+			UECodeGen_Private::ConstructUClass(Z_Registration_Info_UClass_UOverlayWidgetController.OuterSingleton, Z_Construct_UClass_UOverlayWidgetController_Statics::ClassParams);
+		}
+		return Z_Registration_Info_UClass_UOverlayWidgetController.OuterSingleton;
+	}
+	template<> IDLEADVENTURE_API UClass* StaticClass<UOverlayWidgetController>()
+	{
+		return UOverlayWidgetController::StaticClass();
+	}
+	UOverlayWidgetController::UOverlayWidgetController(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {}
+	DEFINE_VTABLE_PTR_HELPER_CTOR(UOverlayWidgetController);
+	UOverlayWidgetController::~UOverlayWidgetController() {}
+	struct Z_CompiledInDeferFile_FID_Users_John_Documents_GitHub_IdleAdventure_IdleAdventure_Source_IdleAdventure_Public_UI_WidgetController_OverlayWidgetController_h_Statics
+	{
+		static const FClassRegisterCompiledInInfo ClassInfo[];
+	};
+	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_John_Documents_GitHub_IdleAdventure_IdleAdventure_Source_IdleAdventure_Public_UI_WidgetController_OverlayWidgetController_h_Statics::ClassInfo[] = {
+		{ Z_Construct_UClass_UOverlayWidgetController, UOverlayWidgetController::StaticClass, TEXT("UOverlayWidgetController"), &Z_Registration_Info_UClass_UOverlayWidgetController, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(UOverlayWidgetController), 3615857320U) },
+	};
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_John_Documents_GitHub_IdleAdventure_IdleAdventure_Source_IdleAdventure_Public_UI_WidgetController_OverlayWidgetController_h_1910269332(TEXT("/Script/IdleAdventure"),
+		Z_CompiledInDeferFile_FID_Users_John_Documents_GitHub_IdleAdventure_IdleAdventure_Source_IdleAdventure_Public_UI_WidgetController_OverlayWidgetController_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Users_John_Documents_GitHub_IdleAdventure_IdleAdventure_Source_IdleAdventure_Public_UI_WidgetController_OverlayWidgetController_h_Statics::ClassInfo),
+		nullptr, 0,
+		nullptr, 0);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ IdleAdventure/Source/IdleAdventure/Public/UI/WidgetController/OverlayWidgetController.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/IdleWidgetController.h"
+#include "OverlayWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IDLEADVENTURE_API UOverlayWidgetController : public UIdleWidgetController
+{
+	GENERATED_BODY()
+	
+};
